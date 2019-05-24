d5c0a363d602493eee7102fdabb30f95e6d13ffe
Hannah Holland-Moritz <hhollandmoritz@gmail.com>
2019-05-23 19:14:33 -0600

    created convert to NovaSeq

--- convert_fastq_to_NovaSeq.py
+++ convert_fastq_to_NovaSeq.py
@@ -15,7 +15,7 @@ learn more about biopython here: https://biopython.org/
 
 import argparse
 from Bio import SeqIO
-import re
+import gzip
 
 
 def main():
@@ -29,7 +29,7 @@ def main():
     pip install biopython
     ''',
     formatter_class=argparse.RawTextHelpFormatter)
-    # what follows is some fancy code so that the required arguments are printed
+    # what follows is some fancy code so /home/hannah/Documents/Fierer_lab/NovaSeqTestdada2that the required arguments are printed
     # above the optional arguments in the --help display.
     parser._action_groups.pop()
     req = parser.add_argument_group('required arguments')
@@ -78,23 +78,29 @@ def main():
     else:
         mod_records = []
 
-    # Get the taxonomy dictionary (before the loop so that it only has to be created once)
-    tax_dict = create_taxonomy_dictionary(map_fp)
-    print("Taxa dictionary created.")
+    # Check if file is compressed or not
+    iscompressed = check_file_compression(input_fp)
+    
+    if iscompressed:
+        input_handle = gzip.open(input_fp, "rt")
+    else:
+        input_handle = input_fp
 
-    print("Modifying sequence headers...")
+
+    print("Modifying sequence quality scores...")
 
     # read through the fastq file, for every record run the modify_fasta_header function
-    for record in SeqIO.parse(input_fp, "fasta"):
+    for record in SeqIO.parse(input_handle, "fastq"):
         # extract header from record
-        header=record.description
+        # print(record.letter_annotations["phred_quality"]) # debugging
+        # print(record) # debugging
 
-        # modify the header to match the sinTAX format, add ";" to sequence id
-        seq_id_out, header_out = modify_fasta_header(header, tax_dict)
+        # modify the quality information in NovaSeq format
+        qual_out = modify_fastq_quality(list(record.seq),
+                                        record.letter_annotations["phred_quality"])
 
-        # overwrite the record description and id with the new header and new sequence id
-        record.description = header_out
-        record.id = seq_id_out
+        # overwrite the record quality score
+        record.letter_annotations["phred_quality"] = qual_out
 
         # save the record to the modified records list;
         # if list is not pre-allocated in size, simply append the records
@@ -122,204 +128,76 @@ def main():
     print("Writing modified headers to file...")
 
     # Write modified records to file
-    with open(output_fp, "w") as handle:
-        SeqIO.write(mod_records, handle, "fasta")
-        #record.description = header_out
+    if iscompressed:
+        with gzip.open(output_fp, "wb") as handle:
+            SeqIO.write(mod_records, handle, "fastq")
+    else:
+        with open(output_fp, "w") as handle:
+            SeqIO.write(mod_records, handle, "fastq")
 
 
 # Helper functions #
-
-
-def create_quality_dictionary():
+def check_file_compression(input_fp):
+    
     """
-    create_quality_dictionary()
-    this function creates a dictionary of quality scores where the keys
-    are traditional illumna fastq quality symbols and the values are the 
-    new NovaSeq quality equivalents. According to illumina 
+    check_file_compression(input_fp)
+    This file uses the gzip "magic number" to check if a file is compressed or
+    not and returns True if it is, or False if it is not.
+    
+    """
+    
+    gzip_magic_number = "1f8b"
+    
+    f = open(input_fp)
+    
+    iscompressed = f.read(2).encode("hex") == gzip_magic_number
+    
+    return iscompressed
+    
+    
+def  modify_fastq_quality(fastq_seq, fastq_qual):
+
+    """
+    modify_fastq_quality(fasta_header)
+    This function takes fastq sequence and fastq traditional numeric quality 
+    score list as input and converts it to the new NovaSeq quality equivalents. 
+    According to illumina 
     (https://www.illumina.com/content/dam/illumina-marketing/documents/products/appnotes/novaseq-hiseq-q30-app-note-770-2017-010.pdf),
     quality scores <15 are assigned 12 ('-'), scores between 15 and 30 
     (inclusive) are assigned 23 ('8'), and scores >30 are assigned 37 ('F'). 
-    Any null scores (i.e. base calls of N) are assigned 2 ('"'). As our pipeline
-    currently filters out Ns before processing, I do not bother assining 2 to
-    no-calls.
+    Any null scores (i.e. base calls of N) are assigned 2 ('"'). 
+    
+    The function does the following steps:
+        1. First checks for the presence of Ns in the sequence.
+        2. If there are Ns, replaces the corresponding quality score with a 2, 
+        otherwise continue to step 3
+        3. Then translate scores <= 2 as 2, >2 and <15 as 12, >=15 and <31 as 
+        23, and any score >30 (i.e. 31 and up) as 37.
+    
     """
-
-    # initialize dictionary
-    qual_dict = {"!" : "-",
-                "\"" : "-",
-                "#" : "-",
-                "$" : "-",
-                "%" : "-",
-                "&" : "-",
-                "'" : "-",
-                "(" : "-",
-                ")" : "-",
-                "*" : "-",
-                "+" : "-",
-                "," : "-",
-                "-" : "-",
-                "." : "-",
-                "/" : "-",
-                "0" : "8",
-                "1" : "8",
-                "2" : "8",
-                "3" : "8",
-                "4" : "8",
-                "5" : "8",
-                "6" : "8",
-                "7" : "8",
-                "8" : "8",
-                "9" : "8",
-                ":" : "8",
-                ";" : "8",
-                "<" : "8",
-                "=" : "8",
-                ">" : "8",
-                "?" : "8",
-                "@" : "F",
-                "A" : "F",
-                "B" : "F",
-                "C" : "F",
-                "D" : "F",
-                "E" : "F",
-                "F" : "F",
-                "G" : "F",
-                "H" : "F",
-                "I" : "F"
-                }
-
-    return qual_dict
-
-
-def multiple_replace(text, adict):
-
-    """
-    multiple_replace(text, adict)
-    takes a text string and a dictionary of replacements
-    allows multiple replacement of characters so that those characters
-    only need to be specified in a dictionary once
-    depends on the re module
-    for more information on this function, see
-    https://www.safaribooksonline.com/library/view/python-cookbook-2nd/0596007973/ch01s19.html
-    """
-    rx = re.compile('|'.join(map(re.escape, adict)))
-
-    def one_xlat(match):
-        return adict[match.group(0)]
-
-    final_replace = rx.sub(one_xlat, text)
-
-    return final_replace
-
-
-def match_taxa(tax_dict, split_taxonomy):
-
-    """
-    match_taxa(tax_dict, split_taxonomy)
-    this function will take the taxonomy dictionary and the
-    split taxa string as input and assign it the proper
-    d:/p:/c:/o:/f:/g:. If a split is the last one in the
-    list, it will be given the species name. Non-conforming
-    taxonomy levels are removed from the list. It will return a
-    list of the taxonomy matches modified to include  d:/p:/c:/o:/f:/g:/s:
-    #
-    Note:
-    this step would probably be better if vectorized, but
-    I don't know how to do that easily, plus I want to keep
-    the script dependencies small. So it is in a loop instead.
-    """
-
-    for taxa_level in list(range(len(split_taxonomy))):
-
-        # save the current taxonomy level as a new variable
-        current_tax_level = split_taxonomy[taxa_level]
-
-        # then if the level is above species, look up it's abbreviation
-        if taxa_level is not (len(split_taxonomy) - 1):
-            # for abbreviations that conform to the sinTax style,
-            # overwrite the split_taxonomy list with the new
-            # style, if they are non-conforming, mark them by
-            # labeling them "non-conforming".
-
-            if tax_dict[current_tax_level] not in ["non-conforming:"]:
-                split_taxonomy[taxa_level] = tax_dict[current_tax_level] + current_tax_level
-            else:
-                split_taxonomy[taxa_level] = "non-conforming"
-
-        # if a taxa is the species level (i.e. the last level in the string)
-        #  put an "s:" before it.
+    # First checks for the presence of Ns in the sequence.
+    if 'N' in fastq_seq:
+    
+       # If there are Ns, replaces the corresponding quality score with a 2
+       for n, i in enumerate(fastq_seq):
+           if i == 'N':
+               fastq_qual[n] = 2
+               
+    # Translate scores           
+    for n, i in enumerate(fastq_qual):
+        if i <= 2:
+            fastq_qual[n] = 2
+        elif i < 15:
+            fastq_qual[n] = 12
+        elif i < 31:
+            fastq_qual[n] = 23
         else:
-            split_taxonomy[taxa_level] = "s:" + current_tax_level
-
-    # remove non-conforming taxonomy levels from the list
-    if "non-conforming" in split_taxonomy:
-        while "non-conforming" in split_taxonomy:
-            split_taxonomy.remove("non-conforming")
-        modified_split_taxonomy = split_taxonomy
-    else:
-        modified_split_taxonomy = split_taxonomy
-
-    return modified_split_taxonomy
+            fastq_qual[n] = 37
 
 
-def modify_fasta_header(fasta_header, tax_dict):
+    modified_fastq_qual = list(fastq_qual)
 
-    """
-    modify_fasta_headers(fasta_header)
-    This function takes a fasta header string as input and a taxonomy dictionary and
-    1) splits the string into the sequence id and the taxonomy string
-    2) splits the string based on semicolon placement
-    3) places d:, p:, c:, o:, f:, g:, and s: at the appropriate places before each
-    taxonomy level with the use of a helper function, match_taxa()
-    4) removes special characters from the taxonomy strings by replacing them with underscores
-    5) rebuilds the string and appends it to the sequence ids
-    6) returns the sequence id with a ";" after it and the modified taxonomy header
-    """
-
-    # First split the header string on the first space and save the
-    # sequence ID and taxonomy [second half]
-    taxonomy_string = fasta_header.split(" ", 1)[1]
-    sequence_id = fasta_header.split(" ", 1)[0]
-
-    # Then split the string based on semicolons
-    split_taxonomy = taxonomy_string.split(";")
-
-    # for each split_taxonomy, check which taxa level it is, and rename it with the proper
-    # d:/p:/c:/o:/f:/g: heading from the taxa_dict (species are not included in that list)
-    # so the last split in the list that does not match any taxa level
-    # will be given the species name.
-    modified_taxa_list = match_taxa(tax_dict, split_taxonomy)
-
-    # Then replace special characters with underscoes
-    replacement_dict = {
-        " " : "_",
-        "(" : "",
-        ")" : "",
-        "/" : "",
-        "," : "_"
-    }
-
-    modified_taxa_list_no_spc_char = []
-
-    for taxa in range(len(modified_taxa_list)):
-        #print(taxa)
-        taxa_name = modified_taxa_list[taxa]
-        #print(taxa_name)
-        taxa_str_no_spc_char = multiple_replace(taxa_name, replacement_dict)
-        #print(taxa_str_no_spc_char)
-        modified_taxa_list[taxa] = taxa_str_no_spc_char
-        #print(modified_taxa_list[taxa])
-
-    # Then rebuild the string from the list by joining taxa list
-    # together with a comma as the separator; also add "tax=" to the beginning
-    full_taxonomy_string = "tax=" + ",".join(modified_taxa_list)
-
-    # now append the modified string to the sequence id
-
-    modified_header = full_taxonomy_string + ";"
-    modified_seq_id = sequence_id + ";"
-
-    return modified_seq_id, modified_header
+    return modified_fastq_qual
 
 
 if __name__ == "__main__":
